0c5842711fa2c7f1538b0d615937b5d99cecd248
adamski83 <adamsik83@wp.pl>
2023-05-22 20:28:46 +0200

    added josn-server and RESTapi methods (post,patch,delete)

--- GroceryList/src/App.jsx
+++ GroceryList/src/App.jsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { apiRequest } from './apiRequest';
 import { Header } from './components/header/Header';
 import { SearchItem } from './components/searchItem/SearchItem';
 import { AddItem } from './components/addItem/AddItem';
@@ -35,23 +36,53 @@ export default function App() {
 		fetchData();
 	}, []);
 
-	const addItem = (item) => {
+	const addItem = async (item) => {
 		const id = items.length ? items[items.length - 1].id + 1 : 1;
 		const myNewItem = { id, checked: false, item };
 		const listItem = [...items, myNewItem];
 
 		setItems(listItem);
+		const postOptions = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(myNewItem),
+		};
+		const result = await apiRequest(API_URL, postOptions);
+		if (result) setFetchError(result);
 	};
 
-	const handleCheck = (id) => {
+	const handleCheck = async (id) => {
 		const listItems = items.map((item) =>
 			item.id === id ? { ...item, checked: !item.checked } : item
 		);
 		setItems(listItems);
+
+		const myItem = listItems.filter((item) => item.id === id);
+		const updateOptions = {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application-json',
+			},
+			body: JSON.stringify({ checked: myItem[0].checked }),
+		};
+		const reqUrl = `${API_URL}/${id}`;
+		const result = await apiRequest(reqUrl, updateOptions);
+		if (result) {
+			setFetchError(result);
+		}
 	};
-	const handleDelete = (id) => {
+	const handleDelete = async (id) => {
 		const listItems = items.filter((item) => item.id !== id);
 		setItems(listItems);
+
+		const deleteOptions = { method: 'DELETE' };
+		const reqUrl = `${API_URL}/${id}`;
+		const result = await apiRequest(reqUrl, deleteOptions);
+		if (result) {
+			setFetchError(result);
+		}
 	};
 
 	const handleSubmit = (e) => {
---
+++ GroceryList/src/apiRequest.js
@@ -0,0 +1,17 @@
+export const apiRequest = async (
+	url = '',
+	optionsObj = null,
+	errMsg = null
+) => {
+	try {
+		// @ts-ignore
+		const response = await fetch(url, optionsObj);
+		if (!response.ok) {
+			throw Error('Please reload the app');
+		}
+	} catch (err) {
+		errMsg = err.message;
+	} finally {
+		return errMsg;
+	}
+};
